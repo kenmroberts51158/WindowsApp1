06f489667dcb6289b7096ea171d9acffb9145e98
Kenneth Roberts <ken.roberts@sullivantire.com>
2018-07-18 14:06:51 -0400

    Add project files.

---
+++ WindowsApp1/DadJoke.vb
@@ -0,0 +1,35 @@
+﻿
+Public Class DadJoke
+
+    Private Shared myJokeArray As String() = System.IO.File.ReadAllLines("C:\Ken\dadjokes.txt")
+    Private Shared myRandomGenerator As Random = New Random()
+
+    Public Shared ReadOnly Property GetInstance As DadJoke
+        Get
+            Static Instance As DadJoke = New DadJoke
+            Return Instance
+        End Get
+    End Property
+
+    Private Sub New()
+        ' prevents object creation
+    End Sub
+
+    Public Function GetJoke() As String
+        Return myJokeArray(myRandomGenerator.Next(0, myJokeArray.Count))
+    End Function
+
+    Public Sub UpdateJokeFile(ByVal filePath As String, ByVal password As String)
+
+        Try
+            If password = "fred" Then
+                myJokeArray = System.IO.File.ReadAllLines(filePath)
+            End If
+        Catch ex As Exception
+            myJokeArray = System.IO.File.ReadAllLines("C:\Ken\dadjokes.txt")
+        End Try
+
+    End Sub
+
+
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports myIO = System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        MsgBox("hello world")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim quoteArray As String() = myIO.File.ReadAllLines("C:\Ken\dadjokes.txt")
+        RichTextBox1.Text = quoteArray(GetRandom(0, quoteArray.Count))
+
+    End Sub
+
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+
+        Dim myRandomGenerator As Random = New Random()
+        Return myRandomGenerator.Next(Min, Max)
+
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Dim myDadJoke As DadJoke = DadJoke.GetInstance
+        RichTextBox1.Text = myDadJoke.GetJoke()
+        myDadJoke = Nothing
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        fd.Title = "Open File Dialog"
+        fd.InitialDirectory = "C:\Ken"
+        fd.Filter = "Text files (*.txt*)|*.txt*|All files (*.*)|*.*"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            Dim myDadJoke As DadJoke = DadJoke.GetInstance
+            myDadJoke.UpdateJokeFile(fd.FileName, "fred")
+            myDadJoke = Nothing
+        End If
+    End Sub
+End Class
